305656bf99d554de6f9be7ce515dce08f03ff65e
He-Martijn <roert.stapje_0o@icloud.com>
2021-11-22 21:07:47 +0100

    Write small explanation what this project is about.

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,17 @@
 import 'package:flutter/material.dart';
 
 // To make sure github and pushing works this comment. 
-
+// Since this is an open source repo. For everyone who reads this :
+// I follow the FLutter course by Angela Yu on github.
+// This lesson we create an app called I am rich.
+// According to an urban legend such an app was sold for
+// way to much money. So people could show off that they where rich.
+// All it does was showing an drawing of a diamond.
+// Anyway consider this app a sort of ' Hello World' app.
+//
+// Needles to say : This project is not going to be published in any app store.
+//
+// Regards, Martijn. 
 
 void main() {
   MaterialApp{
